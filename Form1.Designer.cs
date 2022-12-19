618ef0bfc5fa4962910939f0f96571b0a06997db
m4rcer <m4rcer@gmail.com>
2022-12-19 23:56:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace lab6_g
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1344, 664);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 664);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab6_g
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox.BackColor = Color.Green;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+
+            DrawRoad(g);
+
+            DrawCars(g);
+        }
+
+        private void DrawRoad(Graphics g)
+        {
+            var pbWidth = pictureBox.Size.Width;
+            var pbHeight = pictureBox.Size.Height;
+
+            var pen = new Pen(Color.Black, 2);
+
+            g.DrawLine(pen, new Point(200, pbHeight), new Point(400, 0));
+            g.DrawLine(pen, new Point(pbWidth - 200, pbHeight), new Point(pbWidth - 400, 0));
+
+            g.FillPolygon(Brushes.Gray, new Point[] { new Point(200, pbHeight), new Point(400, 0), new Point(pbWidth - 400, 0), new Point(pbWidth - 200, pbHeight) });
+
+            var whitePen = new Pen(Color.White, 8);
+            whitePen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            g.DrawLine(whitePen, new Point(pbWidth / 2, 0), new Point(pbWidth / 2, pbHeight));
+        }
+
+
+        private void DrawCars(Graphics g)
+        {
+            var car1 = Image.FromFile("car1.png");
+            var car2 = Image.FromFile("car2.png");
+
+            g.DrawImage(car1, 650, 130, 270, 270);
+           g.DrawImage(car2, 450, 350, 270, 195);
+        }
+    }
+}
